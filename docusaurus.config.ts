7a56d57cff532d46fad35792fc30446107e43a10
vinceflores <vincegabriel.flores@gmail.com>
2024-08-01 15:08:16 -0400

    initial

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'vinceflores', // Usually your GitHub org/user name.
-  projectName: 'repos-to-sanity-docs', // Usually your repo name.
+  projectName: 'repo-to-sanity-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
